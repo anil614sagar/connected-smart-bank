0f4e7b3a91a70b6c81ba90d1bdd809aa26536059
anil614sagar <anil614sagar@gmail.com>
2016-05-14 22:08:11 +0530

    Login with mobile working

--- app/passport.js
+++ app/passport.js
@@ -1,12 +1,12 @@
 // config/passport.js
 
 // load all the things we need
-var OAuth2Strategy = require('passport-oauth2');
-// Loading nconf middleware - Used to store, access configurations of webapp
-var nconf = require('nconf');
+var CustomStrategy = require('passport-custom');
+var url = require('url');
+var request = require('request');
 
 
-module.exports = function(passport) {
+module.exports = function (passport) {
 
   // =========================================================================
   // passport session setup ==================================================
@@ -15,30 +15,72 @@ module.exports = function(passport) {
   // passport needs ability to serialize and unserialize users out of session
 
   // used to serialize the user for the session
-  passport.serializeUser(function(user, done) {
+  passport.serializeUser(function (user, done) {
     done(null, user);
   });
   // used to deserialize the user
-  passport.deserializeUser(function(obj, done) {
+  passport.deserializeUser(function (obj, done) {
     done(null, obj);
   });
 
-  //passport.use(new OAuth2Strategy({
-  //    //authorizationURL: 'https://login.uber.com/oauth/v2/authorize',
-  //    //tokenURL: 'https://login.uber.com/oauth/v2/token',
-  //    //clientID: 'jvI9xGMZU24c5qNY6Fhgn7tlPkw03lEC',
-  //    //clientSecret: 'TEgBSTZusGMWQouO2AK0ts67MWwwAM90fEB9IB_4',
-  //    //callbackURL: "http://localhost:3000/auth/uber/callback"
-  //  },
-  //  function(accessToken, refreshToken, profile, cb) {
-  //    console.log(accessToken)
-  //    console.log(refreshToken)
-  //    console.log(profile)
-  //    var user = {}
-  //    user.token = accessToken
-  //    user.refreshToken = refreshToken
-  //    return cb(null, user);
-  //  }
-  //));
+  passport.use(new CustomStrategy(
+    function (req, done) {
+      console.log(req)
+      var apiUrl = req.body['apiUrl']
+      var credentials = req.body['credentials']
+      var verified = true;
+      var messages = [];
+
+      // Verify the OAuth consumer key.
+      if (credentials.indexOf('oauth_consumer_key="3FNFP1C5JZjkQOVX5PeDf7MuG"') == -1) {
+        verified = false;
+        messages.push('The Digits API key does not match.');
+      }
+
+      // Verify the hostname.
+      var hostname = url.parse(req.body.apiUrl).hostname;
+      if (hostname != 'api.digits.com' && hostname != 'api.twitter.com') {
+        verified = false;
+        messages.push('Invalid API hostname.');
+      }
+
+      // Do not perform the request if the API key or hostname are not verified.
+      if (!verified) {
+        return done(true, {
+          phoneNumber: "",
+          userID: "",
+          error: messages.join(' ')
+        })
+      }
+
+      // Prepare the request to the Digits API.
+      var options = {
+        url: apiUrl,
+        headers: {
+          'Authorization': credentials
+        }
+      };
+
+      // Perform the request to the Digits API.
+      request.get(options, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+          // Send the verified phone number and Digits user ID.
+          var digits = JSON.parse(body);
+          return done(null, {
+            phoneNumber: digits.phone_number,
+            userID: digits.id_str
+          });
+        }
+        else {
+          // Send the error.
+          return done(true, {
+            phoneNumber: "",
+            userID: "",
+            error: messages.join(' ')
+          });
+        }
+      });
+    }
+  ));
 
 };
--- app/routes.js
+++ app/routes.js
@@ -1,14 +1,51 @@
 var path = require('path')
 var extend = require('util')._extend
 var request = require('request')
+var url = require('url')
 
 
-
-module.exports = function(app, passport) {
+module.exports = function (app, passport) {
 
   // route to handle all angular requests
   app.get('/', function (req, res) {
     res.sendFile(path.resolve('public/views/landing.html'));
   });
 
+  // user information api call
+  app.get('/user', function (req, res) {
+    if (req.isAuthenticated()) {
+      res.json({'loginStatus': true, 'response': req.user});
+    }
+    else {
+      res.json({'loginStatus': false, 'response': {}});
+    }
+  });
+
+  // route for logging out
+  app.get('/logout', function (req, res) {
+    req.logout();
+    res.redirect('/');
+  });
+
+  /**
+   * POST Digits login.
+   */
+
+  app.post('/digits',
+    passport.authenticate('custom', {failureRedirect: '/'}),
+    function (req, res) {
+      res.redirect('/');
+    }
+  );
+
+}
+
+// route middleware to make sure a user is logged in
+function isLoggedIn(req, res, next) {
+  // if user is authenticated in the session, carry on
+  if (req.isAuthenticated())
+    return next();
+
+  // if they aren't redirect them to the home page
+  res.redirect('/');
 }
