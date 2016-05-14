18ae226e1a808c69bbb7baca95f0dec3481a684f
ndavuluru <ndavuluru@apigee.com>
2016-05-15 01:32:33 +0530

    initial controllers/services

--- public/controllers/dashboardController.js
+++ public/controllers/dashboardController.js
@@ -1,5 +1,7 @@
 
 netbnk.controller('dashboardController', ['$scope', '$stateParams', '$state', function($scope, $stateParams, $state) {
-  $scope.busyApp = true
+  $scope.busyApp = true;
   $scope.dashboard = 'coming very soon...'
+
+  //TODO services
 }])
--- public/controllers/metricsController.js
+++ public/controllers/metricsController.js
@@ -1,5 +1,29 @@
+netbnk.controller('metricsController', ['$scope', '$stateParams', '$state', function ($scope, $stateParams, $state) {
+    $scope.busyApp = true;
+    $scope.metrics = 'coming soon...'
 
-netbnk.controller('metricsController', ['$scope', '$stateParams', '$state', function($scope, $stateParams, $state) {
-  $scope.busyApp = true
-  $scope.metrics = 'coming soon...'
-}])
+    //save metrics
+    $scope.save = function () {
+
+        var investmentData = {
+            bonds: $scope.bonds,
+            stocks: $scope.stocks,
+            insurance: $scope.insurance,
+            mutualFund: $scope.mutualFund,
+            fd: $scope.fd,
+            misc: $scope.misc
+        };
+
+        var spendsData = {
+            rent: $scope.rent,
+            shopping: $scope.shopping,
+            travel: $scope.travel,
+            food: $scope.food,
+            emi: $scope.emi,
+            misc: $scope.misc
+        };
+
+//TODO add services
+
+    }
+}]);
